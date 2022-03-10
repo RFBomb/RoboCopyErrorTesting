4774b074523ee0dd9a5e4663541834162873ca9e
RFBomb <Robbery525@gmail.com>
2022-03-09 19:28:06 -0500

    Add project files.

---
+++ RoboCopyProcess.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RoboSharp;
+
+namespace RoboCopyErrorTesting
+{
+    public class RoboCopyProcess
+    {
+        
+        static RoboSharpConfiguration Configuration { get; } = new RoboSharpConfiguration();
+        static string RoboCopyPath => Configuration.RoboCopyExe;
+
+        public RoboCopyProcess() { }
+        public RoboCopyProcess(string Destination) { Dest = Destination; }
+
+        public string Source { get; set; } = Path.Combine(Directory.GetCurrentDirectory(), "SourceFiles");
+        public string Dest { get; set; } = "C:\\DEST";
+        public List<string> LogLines { get; set; }
+        public List<string> ErrLines { get; set; }
+        public RoboSharpConfiguration Config => Configuration;
+
+        public void RunRoboCopyProcess()
+        {
+            Console.WriteLine("Setting Up RoboCopy Process");
+            LogLines = new List<string>();
+            ErrLines = new List<string>();
+            using (Process RC = new Process())
+            {
+                Console.WriteLine($"- RoboCopy Executable Path: {RoboCopyPath}");
+                RC.StartInfo.FileName = RoboCopyPath;
+                RC.StartInfo.RedirectStandardOutput = true;
+                RC.StartInfo.RedirectStandardError = true;
+                RC.StartInfo.UseShellExecute = false;
+                RC.OutputDataReceived += RC_OutputDataReceived;
+                RC.ErrorDataReceived += RC_ErrorDataReceived;
+                RC.StartInfo.Arguments = $"{Source} {Dest} /S /V /R:0 /W:3";
+                //Start
+                Console.WriteLine($"- Starting RoboCopy Process");
+                bool started = RC.Start();
+                Assert.IsTrue(started);
+                RC.BeginOutputReadLine();
+                RC.BeginErrorReadLine();
+                RC.WaitForExit();
+                Console.WriteLine($"- RoboCopy Process has Exited");
+                RC.Dispose();
+            }
+        }
+
+        private void RC_ErrorDataReceived(object sender, DataReceivedEventArgs e)
+        {
+            if (e?.Data != null)
+                ErrLines.Add(e.Data);
+        }
+
+        private void RC_OutputDataReceived(object sender, DataReceivedEventArgs e)
+        {
+            if (e?.Data != null)
+                LogLines.Add(e.Data);
+        }
+
+        public void WriteLogLines(Exception e)
+        {
+            if (e != null)
+            {
+                Console.WriteLine($"- Assertion Failed: No Error reported in the log.");
+            }
+
+            if (ErrLines.Count > 0)
+            {
+                Console.WriteLine($"- Error Data Received from Process.ErrorDataReceived): \n");
+                foreach (string s in LogLines)
+                    Console.WriteLine(s);
+            }
+            else
+            {
+                Console.WriteLine($"- No Error Data Received from Process (via Process.ErrorDataReceived)");
+            }
+
+
+            Console.WriteLine($"- Writing Log Lines: \n");
+            foreach (string s in LogLines)
+                Console.WriteLine(s);
+
+            if (e != null) throw e;
+        }
+
+        public void AssertErrorGenerated()
+        {
+            try
+            {
+                //If any of the log lines contains 'ERROR', then this will pass the test.
+                //If no error is reported, Assert will throw.
+                Assert.IsTrue(LogLines.Any(str => str.Contains(Config.ErrorToken)));
+                WriteLogLines(null);
+            }
+            catch (Exception e)
+            {
+                WriteLogLines(e);
+            }
+        }
+    }
+
+}
---
+++ RoboCopyValidationUnitTests.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RoboSharp;
+using System;
+using System.IO;
+using System.Linq;
+using System.Diagnostics;
+using System.Collections.Generic;
+
+namespace RoboCopyErrorTesting
+{
+
+    [TestClass]
+    public class RoboCopyValidationUnitTests
+    {
+
+        [TestMethod]
+        public void NoSourceDirectoryTest()
+        {
+            Console.WriteLine("This test is using a Directory path that does not exist as the source, and as such an Error should always be reported.");
+            Console.WriteLine("");
+            Console.WriteLine("If this test passes, an error has been reported by RoboCopy.");
+            Console.WriteLine("If this test fails, RoboCopy reported no errors into the log.");
+            Console.WriteLine("");
+            var RC = new RoboCopyProcess();
+            RC.Source = "C:\\SourceDirDoesNotExist";
+            RC.RunRoboCopyProcess();
+            //Have the RoboCopy class evaluate the results and write out the log lines
+            RC.AssertErrorGenerated();
+        }
+
+        [TestMethod]
+        public void FileLockedTest()
+        {
+            Console.WriteLine("This test locks a file in the destination to force robocopy to report a failure and an error.");
+            Console.WriteLine("");
+            Console.WriteLine("If this test passes, an error has been reported by RoboCopy.");
+            Console.WriteLine("If this test fails, RoboCopy reported no errors into the log.");
+            Console.WriteLine("");
+            RoboCopyProcess RC = new RoboCopyProcess();
+            //Setup Command Options
+            DirectoryInfo d = new DirectoryInfo(RC.Dest);
+            Directory.CreateDirectory(RC.Dest);
+            if (d.Exists)
+            {
+                Console.WriteLine($"Destination Exists : Deleting it to ensure fresh run.");
+                d.Delete(true);
+            }
+
+            //Prep the locked file
+            string LockedFile = Path.Combine(RC.Dest, "1024_Bytes.txt");
+            //FileInfo LFile = new FileInfo(LockedFile);
+            //LFile.Create();
+
+            Console.WriteLine($"Running the Test");
+            Console.WriteLine($"- Creating Destination Directory");
+            d.Create();
+            Console.WriteLine($"- Locking File Path: {LockedFile}");
+            var FStream = new StreamWriter(LockedFile);
+            FStream.WriteLine("This File Is Locked");
+            RC.RunRoboCopyProcess();
+            //Close stream
+            FStream.Close();
+            Console.WriteLine("- Unlocking the Locked file path.");
+            //Have the RoboCopy class evaluate the results and write out the log lines
+            RC.AssertErrorGenerated();
+        }
+    }
+
+}
